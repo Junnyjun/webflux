e4d84f598dae445d188910847986a1ba4a8cbba8
Junnyjun <chbe5082@naver.com>
2024-06-11 12:00:00 +0900

    set detector

--- src/main/kotlin/git/io/netty/executors/ThreadScheduler.kt
+++ src/main/kotlin/git/io/netty/executors/ThreadScheduler.kt
@@ -1,24 +1,29 @@
 package git.io.netty.executors
 
-import java.util.concurrent.Executors
+import io.netty.channel.Channel
+import io.netty.channel.ChannelFuture
+import io.netty.channel.nio.NioEventLoopGroup
+import io.netty.channel.socket.nio.NioSocketChannel
 import java.util.concurrent.ScheduledFuture
 import java.util.concurrent.TimeUnit
 
 
+//fun main(args: Array<String>) {
+//    val executor = Executors.newScheduledThreadPool(10)
+//    val future: ScheduledFuture<*> = executor.schedule(fun() = println("60 seconds later"), 60, TimeUnit.SECONDS)
+//    executor.shutdown()
+//}
+
 fun main(args: Array<String>) {
-    val executor = Executors.newScheduledThreadPool(10)
+    val group = NioEventLoopGroup()
+    val channel: Channel = NioSocketChannel()
+
+    with(channel) {
+        group.register(this)
+
+        eventLoop().scheduleAtFixedRate(fun() = println("Run every 3 seconds"), 3, 3, TimeUnit.SECONDS)
+        eventLoop().schedule(fun(): ChannelFuture? = channel.closeFuture().sync(), 5, TimeUnit.SECONDS)
 
 
-    // 60초 후에 실행될 작업 예약
-    val future: ScheduledFuture<*> = executor.schedule(
-        { println("60 seconds later") },
-        60, TimeUnit.SECONDS
-    )
-
-
-    // 작업 완료 후 리소스를 해제하기 위해 ScheduledExecutorService 종료
-    executor.shutdown()
-}
-
-class ThreadScheduler {
+    }
 }
