dd01e8e3bcac3702f7d1f7ef30d65aa991321b38
Junnyjun <chbe5082@naver.com>
2024-06-14 00:50:36 +0900

    set detector

--- src/main/kotlin/git/io/netty/bootstrap/SampleBootstrap.kt
+++ src/main/kotlin/git/io/netty/bootstrap/SampleBootstrap.kt
@@ -1,4 +1,33 @@
 package git.io.netty.bootstrap
 
-class SampleBootstrap {
+import io.netty.bootstrap.Bootstrap
+import io.netty.buffer.ByteBuf
+import io.netty.channel.ChannelHandlerContext
+import io.netty.channel.ChannelInitializer
+import io.netty.channel.SimpleChannelInboundHandler
+import io.netty.channel.nio.NioEventLoopGroup
+import io.netty.channel.socket.nio.NioSocketChannel
+import java.net.InetSocketAddress
+
+fun main() {
+    val group = NioEventLoopGroup()
+        val bootstrap = Bootstrap()
+        bootstrap.group(group)
+            .channel(NioSocketChannel::class.java)
+            .handler(object : ChannelInitializer<NioSocketChannel>() {
+                override fun initChannel(ch: NioSocketChannel) {
+                    ch.pipeline().addLast(object : SimpleChannelInboundHandler<ByteBuf>() {
+                        override fun channelRead0(ctx: ChannelHandlerContext, msg: ByteBuf) {
+                            println("Received data")
+                        }
+                    })
+                }
+            })
+
+        val future = bootstrap.connect(InetSocketAddress("www.example.com", 80))
+        future.addListener { f ->
+                println("Connection established")
+        }
+        future.sync()
+        group.shutdownGracefully()
 }
